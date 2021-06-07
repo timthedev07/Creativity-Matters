40db579b1d6521083907ace52bd131fb6c75926a
Harvard90873 <65774333+Harvard90873@users.noreply.github.com>
2021-06-07 20:31:15 +0200

    v1.0.0 initial release - optimized youtube api strategy

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import "./css/master.css";
 import React, { useEffect, useState } from "react";
-import { fetchVideo } from "./utils/Youtube";
+import { generateVideo } from "./utils/Youtube";
 
 function App() {
   const [id, setId] = useState<string>("");
+  const [ok, setOk] = useState<boolean>(true);
 
   useEffect(() => {
-    fetchVideo().then((data) => {
+    // make it so it's less likely to fetch a youtube video
+    generateVideo().then((data) => {
+      if (!data) return setOk(false);
       setId(data);
     });
   }, []);
@@ -16,42 +19,51 @@ function App() {
       <div className="app-content">
         <h1 className="heading">Creativity Matters</h1>
         <h1 className="subheading">Do something creative everyday!</h1>
-        <div className="video-player-wrapper">
-          <iframe
-            className="video-player"
-            title="video"
-            src={`https://www.youtube.com/embed/${id}?showinfo=0&enablejsapi=1&origin=http://localhost:3000`}
-            frameBorder="0"
-            allowFullScreen
-          ></iframe>
-        </div>
-      </div>
 
-      {/* https://serpapi.com/search?q=coffee&location=Sweden&google_domain=google.se&gl=se&hl=sv&num=100 */}
-      <div className="all-text-container">
-        <div className="text-block">
-          <h2 className="block-heading">What is creativity?</h2>
-          <p className="block-text">
-            "Creativity is the process of making an idea your own by modifying
-            something that already exists or combining ideas together to make
-            something satisfying."
-          </p>
+        <div className="video-player-wrapper">
+          {ok ? (
+            <iframe
+              className="video-player"
+              title="video"
+              src={`https://www.youtube.com/embed/${id}?showinfo=0&enablejsapi=1&origin=https://creativity-matters.netlify.app`}
+              frameBorder="0"
+              allowFullScreen
+            ></iframe>
+          ) : (
+            <h2
+              style={{ fontSize: "2.5vw" }}
+              id="error-heading"
+              className="video-player"
+            >
+              😭 We are so sorry about the inconvenience, our YouTube quota has
+              exceeded, we can't get any videos for you today. But check out the
+              content below.
+            </h2>
+          )}
         </div>
-        <div className="text-block">
-          <h2 className="block-heading">Why does creativity matter?</h2>
-          <p className="block-text">
-            Creativity allows us to consider things from another perspective,
-            and thus creates more possible ways of overcoming a challenge.
-            Creativity is also the key to innovation, innovators need open minds
-            to create new things. As a matter of fact, statistics show that
-            creative people tend to live longer.
-          </p>
-        </div>
-        <div className="text-block">
-          <h2 className="block-heading">What activities can I do?</h2>
-          <p className="block-text">
-            <ul className="block-list">
-              <li>Do some DIY crafts</li>
+        <div className="all-text-container">
+          <div className="text-block">
+            <h2 className="block-heading">What is creativity?</h2>
+            <p className="block-text">
+              "Creativity is the process of making an idea your own by modifying
+              something that already exists or combining ideas together to make
+              something satisfying."
+            </p>
+          </div>
+          <div className="text-block">
+            <h2 className="block-heading">Why does creativity matter?</h2>
+            <p className="block-text">
+              Creativity allows us to consider things from another perspective,
+              and thus creates more possible ways of overcoming a challenge.
+              Creativity is also the key to innovation, innovators need open
+              minds to create new things. As a matter of fact, statistics show
+              that creative people tend to live longer.
+            </p>
+          </div>
+          <div className="text-block">
+            <h2 className="block-heading">What activities can I do?</h2>
+            <ul className="block-text">
+              <li>Make some DIY crafts</li>
               <li>
                 Read some interesting books(or even start to write your own
                 stories)
@@ -61,8 +73,9 @@ function App() {
               <li>Write a poem</li>
               <li>LEGO</li>
               <li>Design a merch</li>
+              <li>Create playlists based on the style of different songs</li>
             </ul>
-          </p>
+          </div>
         </div>
       </div>
     </div>
--- src/utils/Youtube.tsx
+++ src/utils/Youtube.tsx
@@ -1,13 +1,94 @@
-const COUNT = 10;
-const API_KEY: string = "AIzaSyCVNxhhhPOyU1UZM-TSOSJIbvEoktmPQrE";
-const BASE_SEARCHING_URL: string = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&maxResults=${COUNT}&part=snippet&type=video&q=`;
+const COUNT = 4;
 
-export const fetchVideo = async () => {
-  const query = "creative%20hack";
-  const res = await fetch(BASE_SEARCHING_URL + query);
-  if (!res.ok) {
-    return "0";
+const API_KEYS = [
+  "AIzaSyCVNxhhhPOyU1UZM-TSOSJIbvEoktmPQrE",
+  "AIzaSyDULCMvpdgt4dsgzSgkJ5Zdc5CxYaqD8bI",
+  "AIzaSyAHNkGKkRDhPfssGKfQg-nj8h8nTkp1jr0",
+  "AIzaSyBGkHC6ec_X8gtE-NaOCZKUGDPkvYppobs",
+];
+
+const POSSIBLE_KEYWORDS = [
+  "DIY",
+  'poem writing "tutorial"',
+  'piano "tutorial"',
+  "life hack",
+  "lego tricks",
+  "football tricks",
+  "basketball tricks",
+  "badminton tricks",
+  "english language tricks",
+  "skateboard tricks",
+  "merch design",
+  "how to compose a piece of music",
+  "rearrange your room | redesign your room",
+  "how to paint a room",
+  "DIY Galaxy Night Lamp tutorial",
+  "school life hacks",
+  "yoga tutorial",
+  "calligraphy tutorial",
+  "swimming with sharks",
+  `deep sea fishing "tutorial"`,
+  `how to decorate a christmas tree`,
+  "how to make summery drinks",
+];
+
+export const generateVideo = async () => {
+  const today = new Date();
+  if (localStorage.getItem("id")) {
+    const lastDate = new Date(
+      localStorage.getItem("lastDate") ||
+        `${today.getMonth() + 1} ${today.getDate()} ${today.getFullYear()}`
+    );
+    if (Math.abs(lastDate.valueOf() - today.valueOf()) < 86400000) {
+      return localStorage.getItem("id");
+    }
   }
+  const query = generateQuery();
+  const res = await tryGetVideo(query, 0);
+  return res ? res : null;
+};
+
+/**
+ * Recursively trying to get a video using multiple api keys, returns null if all of them failed.
+ * @param query
+ * @param n
+ * @returns
+ */
+const tryGetVideo: (query: string, n: number) => Promise<string> = async (
+  query: string,
+  n: number
+) => {
+  if (n >= API_KEYS.length) return null;
+  const res = await fetch(getBaseUrl(API_KEYS[n]) + query);
+  if (!res.ok) {
+    return tryGetVideo(query, n + 1);
+  }
+  const today = new Date();
   const obj = await res.json();
-  return obj.items[0].id.videoId;
+  const id = obj.items[Math.ceil(Math.random() * (COUNT - 1))].id.videoId;
+  localStorage.setItem("id", id);
+  localStorage.setItem(
+    "lastDate",
+    `${today.getMonth() + 1} ${today.getDate()} ${today.getFullYear()}`
+  );
+  return id;
+};
+
+const generateQuery = () => {
+  const ind = Math.ceil(Math.random() * (POSSIBLE_KEYWORDS.length - 1));
+  const choice = POSSIBLE_KEYWORDS[ind];
+  return choice;
+};
+
+/**
+ * Generates a random boolean
+ * The larger the parameter `num` is, the more possible it is to return true;
+ * @param num a number between 1 - 10
+ */
+export const weightedBool = (num: number) => {
+  return Math.random() * 10 < num;
+};
+
+const getBaseUrl = (key: string) => {
+  return `https://www.googleapis.com/youtube/v3/search?key=${key}&maxResults=${COUNT}&part=snippet&type=video&q=`;
 };
